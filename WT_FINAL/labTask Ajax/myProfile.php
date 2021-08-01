6f636b74c8eaccfed6bc72601ff9de96c75c01ef
Your Name <you@example.com>
2021-08-01 15:39:44 +0600

    First part

---
+++ WT_FINAL/labTask Ajax/myProfile.php
@@ -0,0 +1,13 @@
+<html>
+    <head> </head>
+    <?php $name = "sabbir";?>
+    <body>
+        <h1 align="center">My Profile</h1>
+        <table align="center">
+            <tr>
+                <td><?php echo $name;?></td>
+            </tr>
+
+        </table>
+    </body>
+    </html>
